8dbca619c3bc03dc3fe09c782ca44a059d9f1728
hanaori <pa.pi.pu.pe.puwapuwapu@gmail.com>
2016-03-06 17:12:34 +0900

    BoardSpecのdescriveとit実装

---
+++ KPTerTests/BoardSpec.swift
@@ -0,0 +1,30 @@
+//
+//  BoardSpec.swift
+//  KPTer
+//
+//  Created by Kaori Sawamura on 3/6/16.
+//  Copyright © 2016 HanaLucky. All rights reserved.
+//
+
+import Quick
+import Nimble
+import Board
+
+class BoardSpec: QuickSpec {
+    
+    override func spec() {
+        describe("新規Boardを作成する newメソッド") {
+            it("新規Boardを作成できること") {
+                
+            }
+            it("Boardのboard_titleが引数に指定した文字列であること") {
+                
+            }
+        }
+        describe("既存のBoardを編集する editメソッド") {
+            it("Boardの名前を引数に指定し、Boardの名前を変更できること") {
+                
+            }
+        }
+    }
+}
