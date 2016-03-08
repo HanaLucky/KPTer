115d9c0c230f777a2b14062938d70eb7dc26fcf2
hanaori <pa.pi.pu.pe.puwapuwapu@gmail.com>
2016-03-08 21:55:13 +0900

    Cardスペック（describeとit）実装しました

---
+++ KPTerTests/CardSpec.swift
@@ -0,0 +1,39 @@
+import Quick
+import Nimble
+
+class CardSpec: QuickSpec {
+    override func spec() {
+        describe("新規Cardを作成する") {
+            it("新規Cardを作成できること") {
+                
+            }
+            it("Cardのcard_titleが引数に指定した文字列であること") {
+                
+            }
+        }
+        
+        describe("既存のCardを編集する") {
+            it("Cardの名前を引数に指定し、Cardの名前を変更できること") {
+                
+            }
+        }
+        
+        describe("種別がKeepであるか判別") {
+            it("種別がKeepである") {
+                
+            }
+        }
+        
+        describe("種別がProblemであるか判別") {
+            it("種別がProblemである") {
+                
+            }
+        }
+        
+        describe("種別がTryであるか判別") {
+            it("種別がTryである") {
+                
+            }
+        }
+    }
+}
