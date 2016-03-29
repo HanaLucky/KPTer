fe58684525c7f732994493a11d7ab570afa998b0
hanaori <pa.pi.pu.pe.puwapuwapu@gmail.com>
2016-03-29 22:14:43 +0900

    種別ごとにBoardに紐付いたCardを検索するメソッドを実装

--- KPTer/BoardViewModel.swift
+++ KPTer/BoardViewModel.swift
@@ -38,12 +38,32 @@ class BoardViewModel {
         }
     }
     
-    class func addCard(board: Board, title: String, detail: String) {
-        let card = CardViewModel.create(title, detail: detail)
+    class func addCard(board: Board, title: String, detail: String, type: Card.CardType) {
+        let card = CardViewModel.create(title, detail: detail, type: type)
         let realm = try! Realm()
         try! realm.write {
             board.cards.append(card!)
         }
+    }
+    
+    // Boardに紐づくKeep Cardを取得する
+    class func findKeepCard(board: Board) -> Results<Card> {
+        return findCardByType(board, type: Card.CardType.Keep)
+    }
+    
+    // Boardに紐づくProblem Cardを取得する
+    class func findProblemCard(board: Board) -> Results<Card> {
+        return findCardByType(board, type: Card.CardType.Problem)
+    }
+    
+    // Boardに紐づくTry Cardを取得する
+    class func findTryCard(board: Board) -> Results<Card> {
+        return findCardByType(board, type: Card.CardType.Try)
+    }
+    
+    // Boardに紐づくCardをタイプ別に取得する
+    private class func findCardByType(board: Board, type: Card.CardType) -> Results<Card> {
+        return board.cards.filter("type = '\(type.rawValue)'")
     }
 
 }
