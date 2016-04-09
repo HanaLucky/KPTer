d9bed77e805352db6a238e6ce615d96392d95379
hanaori <pa.pi.pu.pe.puwapuwapu@gmail.com>
2016-04-10 01:24:47 +0900

    cardの表示順を変更するメソッドを実装

--- KPTerTests/BoardSpec.swift
+++ KPTerTests/BoardSpec.swift
@@ -15,100 +15,125 @@ import Nimble
 // BoardViewModelのテストをする際は、CardSpecのspec()内をコメントアウトしてください。
 class BoardSpec: QuickSpec {
     
-//    override func spec() {
-//        
-//        let realm = try! Realm()
-//        try! realm.write {
-//            realm.deleteAll()
-//        }
-//        var newBoard: Board? = BoardViewModel.create("new board title")
-//        //BoardViewModel.addCard(newBoard!, title: "newBoard card title", detail: "newBoard card detail")
-//        
-//        describe("新規Boardを作成する") {
-//            it("新規Boardを作成できること") {
-//                expect(newBoard).toNot(beNil())
+    override func spec() {
+        
+        let realm = try! Realm()
+        try! realm.write {
+            realm.deleteAll()
+        }
+        var newBoard: Board? = BoardViewModel.create("new board title")
+        //BoardViewModel.addCard(newBoard!, title: "newBoard card title", detail: "newBoard card detail")
+        
+        describe("新規Boardを作成する") {
+            it("新規Boardを作成できること") {
+                expect(newBoard).toNot(beNil())
+            }
+            it("Boardのboard_titleが引数に指定した文字列であること") {
+                expect(newBoard!.board_title).to(equal("new board title"))
+            }
+        }
+
+        describe("Cardの名前と種別を引数に指定し、Boardに紐付いたCardを作成する") {
+            sleep(2)
+            BoardViewModel.addKeepCard(newBoard!, title: "new card title", detail: "new card detail")
+            BoardViewModel.addProblemCard(newBoard!, title: "new card title2", detail: "new card detail")
+            BoardViewModel.addTryCard(newBoard!, title: "new card title3", detail: "new card detail", fromCard: newBoard!.cards.first!)
+            
+            it("Boardに紐付いたCardが取得できること") {
+                expect(newBoard!.cards).toNot(beNil())
+            }
+//            it("Tryカードのorderが１であること") {
+//                expect(BoardViewModel.findKeepCard(newBoard!).first!.order).to(equal(1))
 //            }
-//            it("Boardのboard_titleが引数に指定した文字列であること") {
-//                expect(newBoard!.board_title).to(equal("new board title"))
-//            }
-//        }
-//
-//        describe("Cardの名前を引数に指定し、Boardに紐付いたCardを作成する") {
-//            sleep(2)
-//            BoardViewModel.addKeepCard(newBoard!, title: "new card title", detail: "new card detail")
-//            BoardViewModel.addProblemCard(newBoard!, title: "new card title2", detail: "new card detail")
-//            BoardViewModel.addTryCard(newBoard!, title: "new card title3", detail: "new card detail", fromCard: newBoard!.cards.first!)
-//            
-//            it("Boardに紐付いたCardが取得できること") {
-//                expect(newBoard!.cards).toNot(beNil())
-//            }
-//            
-//        }
-//        
-//        describe("Cardのリレーションを処理する") {
+        }
+        
+        describe("Cardのリレーションを処理する") {
+            sleep(3)
+                it("引数に渡したcardのidとrelationのidが一致すること") {
+                let realm = try! Realm()
+                expect(newBoard!.cards.first!.id).to(equal(realm.objects(CardRelation).first?.from_id))
+            }
+            
+        }
+        
+        describe("Cardの表示順を変更する") {
+            sleep(3)
+            it("引数に渡したcardの元") {
+                BoardViewModel.addKeepCard(newBoard!, title: "new card title2", detail: "new card detail")
+                BoardViewModel.addKeepCard(newBoard!, title: "new card title3", detail: "new card detail")
+                let cards = BoardViewModel.findKeepCard(newBoard!).sorted("order")
+                let secondCard = cards[1]
+                BoardViewModel.changeCardOrder(cards, from_card_row: 1, to_card_row: 0)
+                expect(BoardViewModel.findKeepCard(newBoard!).sorted("order").first!.id).to(equal(secondCard.id))
+            }
+            
+        }
+        
+        describe("Boardに紐づくKeepのCardを取得する") {
+            sleep(3)
+            let results = BoardViewModel.findKeepCard(newBoard!)
+            it("引数に指定した種別のCardであること") {
+                expect(results.first!.type).to(equal("Keep"))
+            }
+
+        }
+        
+        describe("Boardに紐づくProblemのCardを取得する") {
+            sleep(3)
+            let results = BoardViewModel.findProblemCard(newBoard!)
+            it("引数に指定した種別のCardであること") {
+                expect(results.first!.type).to(equal("Problem"))
+            }
+            
+        }
+        
+        describe("Boardに紐づくTryのCardを取得する") {
+            sleep(3)
+            let results = BoardViewModel.findTryCard(newBoard!)
+            it("引数に指定した種別のCardであること") {
+                expect(results.first!.type).to(equal("Try"))
+            }
+            
+        }
+
+//        // findCardByType(board: Board, type: Card.CardType) @BoardViewModelをテスト
+//        describe("Boardに紐づく引数に指定した種別のCardを取得する") {
 //            sleep(3)
-//                it("引数に渡したcardのidとrelationのidを比較") {
-//                let realm = try! Realm()
-//                expect(newBoard!.cards.first!.id).to(equal(realm.objects(CardRelation).first?.from_id))
-//            }
-//            
-//        }
-//        
-//        describe("Boardに紐づくKeepのCardを取得する") {
-//            sleep(3)
-//            let results = BoardViewModel.findKeepCard(newBoard!)
+//            let results = BoardViewModel.findCardByType(newBoard!, type: Card.CardType.Keep)
 //            it("引数に指定した種別のCardであること") {
 //                expect(results.first!.type).to(equal("Keep"))
 //            }
-//
-//        }
-//        
-//        describe("Boardに紐づくProblemのCardを取得する") {
-//            sleep(3)
-//            let results = BoardViewModel.findProblemCard(newBoard!)
-//            it("引数に指定した種別のCardであること") {
-//                expect(results.first!.type).to(equal("Problem"))
-//            }
 //            
 //        }
-//        
-//        describe("Boardに紐づくTryのCardを取得する") {
-//            sleep(3)
-//            let results = BoardViewModel.findTryCard(newBoard!)
-//            it("引数に指定した種別のCardであること") {
-//                expect(results.first!.type).to(equal("Try"))
-//            }
-//            
-//        }
-//
-////        // findCardByType(board: Board, type: Card.CardType) @BoardViewModelをテスト
-////        describe("Boardに紐づく引数に指定した種別のCardを取得する") {
-////            sleep(3)
-////            let results = BoardViewModel.findCardByType(newBoard!, type: Card.CardType.Keep)
-////            it("引数に指定した種別のCardであること") {
-////                expect(results.first!.type).to(equal("Keep"))
-////            }
-////            
-////        }
-//
-//        describe("既存のBoardを編集する") {
+
+        describe("既存のBoardを編集する") {
+            sleep(2)
+            it("Boardの名前を引数に指定し、Boardの名前を変更できること") {
+                BoardViewModel.edit(newBoard!, title: "edit board title")
+                let editBoard: Board? = realm.objects(Board).first
+                expect(editBoard!.board_title).to(equal("edit board title"))
+            }
+        }
+        
+//        describe("種別ごとに表示順をソートする") {
 //            sleep(2)
+//            changeWholeCardsOrder(newBoard.cards, cards: List<Card>)
 //            it("Boardの名前を引数に指定し、Boardの名前を変更できること") {
 //                BoardViewModel.edit(newBoard!, title: "edit board title")
 //                let editBoard: Board? = realm.objects(Board).first
 //                expect(editBoard!.board_title).to(equal("edit board title"))
 //            }
 //        }
-//
-//        describe("Boardを削除する") {
-//            
-//            it("削除されたBoardが取得できないこと/削除されたBoardに紐づくcardsが取得できないこと") {
-//                sleep(10)
-//                BoardViewModel.delete(newBoard!)
-//                expect(realm.objects(Board).count).to(equal(0)) //削除されたBoardが取得できないこと
-//                expect(realm.objects(Card).count).to(equal(0))  //削除されたBoardに紐づくcardsが取得できないこと
-//            }
-//        }
-//    }
+
+        describe("Boardを削除する") {
+            
+            it("削除されたBoardが取得できないこと/削除されたBoardに紐づくcardsが取得できないこと") {
+                sleep(10)
+                BoardViewModel.delete(newBoard!)
+                expect(realm.objects(Board).count).to(equal(0)) //削除されたBoardが取得できないこと
+                expect(realm.objects(Card).count).to(equal(0))  //削除されたBoardに紐づくcardsが取得できないこと
+            }
+        }
+    }
 }
  
--- KPTerTests/CardSpec.swift
+++ KPTerTests/CardSpec.swift
@@ -6,56 +6,56 @@ import Nimble
 
 // CardViewModelのテストをする際は、BoardSpecのspec()内をコメントアウトしてください。
 class CardSpec: QuickSpec {
-    override func spec() {
-        
-        let realm = try! Realm()
-        var newCard: Card? = CardViewModel.create("new card title", detail: "new card detail", type: Card.CardType.Keep)
-        
-        describe("新規Cardを作成する") {
-            it("新規Cardを作成できること") {
-                expect(newCard).toNot(beNil())
-            }
-            it("Cardのcard_titleが引数に指定した文字列であること") {
-                expect(newCard!.card_title).to(equal("new card title"))
-            }
-            it("Cardのdetailが引数に指定した文字列であること") {
-                expect(newCard!.detail).to(equal("new card detail"))
-            }
-            it("Cardのstatusが'Open'であること") {
-                expect(newCard!.status).to(equal("Open"))
-            }
-            it("Cardのtypeが'Keep'であること") {
-                expect(newCard!.type).to(equal("Keep"))
-            }
-        }
-        
-        describe("既存のCardを編集する") {
-            it("Cardの名前を引数に指定し、Cardの名前を変更できること") {
-                sleep(3)
-                CardViewModel.edit(newCard!, title: "edit card title", detail: "edit card detail")
-                let editCard: Card? = realm.objects(Card).first
-                expect(editCard!.card_title).to(equal("edit card title"))
-            }
-        }
-        
-        describe("Cardのstatusを編集する") {
-            it("CardとCardのstatusを引数に渡すと、cardのstatusがその値になること") {
-                sleep(5)
-                CardViewModel.changeStatus(newCard!, status: Card.CardStatus.Done)
-                let changedStatusCard: Card? = realm.objects(Card).first
-                expect(changedStatusCard!.status).to(equal("Done"))
-            }
-        }
-
-        
-        describe("Cardを削除する") {
-            
-            it("削除されたCardが取得できないこと") {
-                sleep(7)
-                CardViewModel.delete(newCard!)
-                expect(realm.objects(Card).count).to(equal(0)) 
-            }
-        }
-        
-    }
+//    override func spec() {
+//        
+//        let realm = try! Realm()
+//        var newCard: Card? = CardViewModel.create("new card title", detail: "new card detail", type: Card.CardType.Keep)
+//        
+//        describe("新規Cardを作成する") {
+//            it("新規Cardを作成できること") {
+//                expect(newCard).toNot(beNil())
+//            }
+//            it("Cardのcard_titleが引数に指定した文字列であること") {
+//                expect(newCard!.card_title).to(equal("new card title"))
+//            }
+//            it("Cardのdetailが引数に指定した文字列であること") {
+//                expect(newCard!.detail).to(equal("new card detail"))
+//            }
+//            it("Cardのstatusが'Open'であること") {
+//                expect(newCard!.status).to(equal("Open"))
+//            }
+//            it("Cardのtypeが'Keep'であること") {
+//                expect(newCard!.type).to(equal("Keep"))
+//            }
+//        }
+//        
+//        describe("既存のCardを編集する") {
+//            it("Cardの名前を引数に指定し、Cardの名前を変更できること") {
+//                sleep(3)
+//                CardViewModel.edit(newCard!, title: "edit card title", detail: "edit card detail")
+//                let editCard: Card? = realm.objects(Card).first
+//                expect(editCard!.card_title).to(equal("edit card title"))
+//            }
+//        }
+//        
+//        describe("Cardのstatusを編集する") {
+//            it("CardとCardのstatusを引数に渡すと、cardのstatusがその値になること") {
+//                sleep(5)
+//                CardViewModel.changeStatus(newCard!, status: Card.CardStatus.Done)
+//                let changedStatusCard: Card? = realm.objects(Card).first
+//                expect(changedStatusCard!.status).to(equal("Done"))
+//            }
+//        }
+//
+//        
+//        describe("Cardを削除する") {
+//            
+//            it("削除されたCardが取得できないこと") {
+//                sleep(7)
+//                CardViewModel.delete(newCard!)
+//                expect(realm.objects(Card).count).to(equal(0)) 
+//            }
+//        }
+//        
+//    }
 }
