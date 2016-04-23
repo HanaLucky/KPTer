9d836c692d9a1787a5d3f3b54f2bf229295b1706
hanaori <pa.pi.pu.pe.puwapuwapu@gmail.com>
2016-04-23 16:53:33 +0900

    Board編集画面のSaveボタンの挙動を実装

--- KPTer/BoardEditViewController.swift
+++ KPTer/BoardEditViewController.swift
@@ -9,24 +9,49 @@
 import UIKit
 import RealmSwift
 
-class BoardEditViewController: UIViewController {
+class BoardEditViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var boardTitleField: UITextField!
     
     var board: Board? = nil
+    
+    // saveボタン
+    @IBOutlet weak var save: UIBarButtonItem!
     
     // 画面遷移の識別子(ボード一覧のAddから来たかEditからきたか判別)
     var identifier: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // selfをデリゲートにする
+        self.boardTitleField.delegate = self
+        
         // Do any additional setup after loading the view.
         // @TODO implements
         if let boardEntity = board {
             self.boardTitleField.text = boardEntity.board_title
         }
+        
+        // saveボタン非活性
+        if (boardTitleField.text! == "") {
+            save.enabled = false
+        }
     }
-
+    
+    // UITextFieldが編集されると呼ばれるデリゲートメソッド
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        var textLength: Int;
+        textLength = ( textField.text!.characters.count - range.length ) + string.characters.count;
+        if (textLength != 0) {
+            save.enabled = true
+        } else {
+            save.enabled = false
+        }
+        print(boardTitleField.text)
+        return true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -38,8 +63,9 @@ class BoardEditViewController: UIViewController {
         } else if (Identifiers.isBoardEdit(self.identifier)) {
             let editBoard = Board()
             editBoard.board_title = self.boardTitleField.text!
-            BoardViewModel.edit(board!, editBoard: editBoard)            
+            BoardViewModel.edit(board!, editBoard: editBoard)
         }
+        
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
